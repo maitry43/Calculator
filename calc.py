6c74abcf2013102b57f1da4b2f3662b1e8c08a4b
maitry43 <104255567+maitry43@users.noreply.github.com>
2022-05-24 21:56:44 +0530

    Updated functions

--- calc.py
+++ calc.py
@@ -5,10 +5,17 @@ def subtract(x,y):
   return x-y
 
 def multiply(x,y):
-  pass
+  return x*y
 
 def square(x,y):
   pass
 
 def divide(x,y):
+  pass
+
+def print_name(x):
+  print(x)
+  pass
+
+def exp(x,y)
   pass
